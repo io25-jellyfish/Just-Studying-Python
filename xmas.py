e78c9422ba2e1218ffa3c61d041214e64936ca15
Io <95838588+io25-jellyfish@users.noreply.github.com>
2021-12-27 00:53:50 +0900

    Add flie : xmas
    Just made a single Christmas function to pratice def

---
+++ xmas.py
@@ -0,0 +1,10 @@
+#Please type xmas_eng() if you can speak English!
+#한글을 사용하신다면 함수 사용 시에는 xmas_kor()를 입력해주세요!
+
+def xmas_kor():
+    x = input('당신의 닉네임을 알려주세요!')
+    print(f'Merry Christmas, {x}님!')
+
+def xmas_eng():
+    a = input('Please type your nickname here!')
+    print(f'Merry Christmas, {a}!')
